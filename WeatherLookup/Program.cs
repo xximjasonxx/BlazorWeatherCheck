afcc87d522b038f087f18157ada02bfec0519b74
Jason Farrell <imjason@gmail.com>
2018-05-15 02:42:43 +0000

    some changes areound data project

--- WeatherLookup/Program.cs
+++ WeatherLookup/Program.cs
@@ -9,9 +9,12 @@ namespace WeatherLookup
     {
         static void Main(string[] args)
         {
+            
+
             var serviceProvider = new BrowserServiceProvider(services =>
             {
                 // Add any custom services here
+                
             });
 
             new BrowserRenderer(serviceProvider).AddComponent<App>("app");
