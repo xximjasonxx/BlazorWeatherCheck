f95c136aa225db5b9343de6f596169a76204f965
Jason Farrell <imjason@gmail.com>
2018-05-20 04:59:27 +0000

    UI arrangement

---
+++ WeatherLookup/ProcessController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherLookup
+{
+    public static class ProcessController
+    {
+        public static string GetWeatherIconImageUrl(int iconNumber)
+        {
+            return string.Format("https://developer.accuweather.com/sites/default/files/{0}-s.png",
+                iconNumber.ToString().PadLeft(2, '0'));
+        }
+    }
+}
